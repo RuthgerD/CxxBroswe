4813ad9cd3bc0262d493e1b7c54dd03b85834a17
Leith Hobson <leith@student.chalmers.se>
2020-10-20 23:24:51 +0200

    [Backend] - User controller - There appears to be something of a bad rebase here, that has duplicated the isSameUser() method

--- server/controllers/user.mjs
+++ server/controllers/user.mjs
@@ -16,14 +16,6 @@ export default class UserController extends default_metacontroller( UserService 
     return res.status(200).json(user);
   }
 
-  static async isSameUser(req, user) {
-    return req.requestUser === user;
-  }
-
-  static async isSameUser(req, user) {
-    return req.requestUser === user;
-  }
-
   static async remove(req, res) {
     const user = await UserService.get(req.params.id);
     if (!user) return res.status(404).json({ message: "User does not exist" });
