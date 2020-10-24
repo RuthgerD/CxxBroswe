959f593a5db834e1e9fa28619322d48db52b9bd1
Janson R <470-dryan@users.noreply.git.chalmers.se>
2020-10-25 01:44:41 +0200

    [backend] object-flatten.mjs: handle arrays

--- server/src/object-flatten.mjs
+++ server/src/object-flatten.mjs
@@ -4,6 +4,8 @@ Object.flatten = (obj, roots = [], separator = '.') => Object.keys(obj)
         memo,
         Object.prototype.toString.call(obj[prop]) === '[object Object]'
             ? Object.flatten(obj[prop], roots.concat([prop]))
+            : Array.isArray(obj[prop])
+            ? Object.flatten(obj[prop].reduce((acc, e, i) => (acc[i] = e, acc), {}), roots.concat([prop]))
             : {[roots.concat([prop]).join(separator)]: obj[prop]}
     ), {});
 
